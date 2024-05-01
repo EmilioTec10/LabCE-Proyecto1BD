53e0063cac2427efe05582a659e1e38fdbc76925
mrr79 <marianarojas2197@gmail.com>
2024-04-30 23:51:47 -0600

    i

--- LabCEAPI/LabCEAPI/Reportes de Horas/ReporteGeneral.cs
+++
@@ -1,9 +0,0 @@
-﻿using LabCEAPI.Laboratorios;
-
-namespace LabCEAPI.Reportes_de_Horas
-{
-    public class ReporteGeneral
-    {
-        LinkedList<ReporteOperador> reporte_operadores = new LinkedList<ReporteOperador>();
-    }
-}
--- SQLQuery4.sql
+++
@@ -1,6 +0,0 @@
-
-USE LabCE;
-
--- Agregar la columna email_op a la tabla Reserva y establecerla como una clave externa (para palmadas)
-ALTER TABLE Reserva
-ADD email_op VARCHAR(50) FOREIGN KEY REFERENCES Operador(email_op);
